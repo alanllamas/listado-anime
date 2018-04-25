2a65ad21f159db221266360d08ca4a8d390c3198
alanllamas <alan@alans-MacBook-Pro.local>
2018-04-24 21:28:48 -0500

    se cambian templates

--- anime-app/src/app/app.module.ts
+++ anime-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+
 import { AppComponent } from './app.component';
 import { AnimeComponent } from './anime/anime.component';
 import { CoreModule } from './core/core.module';
 import { DetalleAnimeComponent } from './detalle-anime/detalle-anime.component';
+import { AppRouterModule } from './app-router/app-router.module';
 
 
 @NgModule({
@@ -16,7 +18,8 @@ import { DetalleAnimeComponent } from './detalle-anime/detalle-anime.component';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    AppRouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
